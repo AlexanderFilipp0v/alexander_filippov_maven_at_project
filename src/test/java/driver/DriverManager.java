ac04f77639a0f06c10d5f5a1258559787e2d3295
AlexanderFilipp0v <yozhick77@gmail.com>
2023-05-06 10:03:49 +0300

    Made corrections

---
+++ src/test/java/driver/DriverManager.java
@@ -0,0 +1,32 @@
+package driver;
+
+import day17.Config;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+
+    public static WebDriver getDriver(Config config) {
+        return switch (config) {
+            case CHROME -> getChromeDriver();
+            case FF -> getFirefoxDriver();
+            case EDGE -> getEDGEDriver();
+            //default -> throw null;
+        };
+    }
+
+    private static WebDriver getChromeDriver() {
+        return new ChromeDriver();
+
+    }
+
+    private static WebDriver getFirefoxDriver() {
+        return null;
+
+    }
+
+    private static WebDriver getEDGEDriver() {
+
+        return null;
+    }
+}
